3ce922ba5f55b1485e1d419795767c302ac72067
Lyall <58636255+Lyall-A@users.noreply.github.com>
2024-01-10 16:57:17 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const config = require(`${__dirname}/config.json`); // Config
-const https = require("https"); // HTTPS module to send typing requests
+const https = require("https"); // HTTPS module
 if (!config.token) return console.log("No token provided, please get your Discord token and add it to the config.json file next to \"token\"!"); // If token not provided
-if (!config.channel) return console.log("No channel ID(s) provided, please get 1 or more channel ID(s) and add it to the config.json file next to \"channel\"! Example: \x1b[100m\"channel\": [\"1234567890123456789\", \"9876543210987654321\"]\x1b[0m"); // If channel ID(s) not provided
-const isArray = typeof config.channel === "object" ? true : false; // Checks if provided channel ID(s) is an array or a string
-if (isArray && !config.channel[0]) return console.log("No channel ID(s) provided, please get 1 or more channel ID(s) and add it to the config.json file next to \"channel\"! Example: \x1b[100m\"channel\": [\"1234567890123456789\", \"9876543210987654321\"]\x1b[0m"); // If channel is array but no channel ID's inside
+if (!config.channel?.[0]) return console.log("No channel ID(s) provided, please get 1 or more channel ID(s) and add it to the config.json file next to \"channel\"! Example: \x1b[100m\"channel\": [\"1234567890123456789\", \"9876543210987654321\"]\x1b[0m"); // If channel ID(s) not provided
+if (config.channel === "string") config.channel = [config.channel]; // Converts single channel ID string to array
 
-console.log(`Started! Channel ID${isArray ? config.channel[1] ? "'s" : "" : ""}: ${isArray ? config.channel.join(", ") : config.channel}`); // Log channel ID(s)
+console.log(`Started! Channel ID(s): ${config.channel.join(", ")}`); // Log channel ID(s)
 
 process.on("uncaughtException", (err) => {
     // On rejection
-    console.log(`Error, please make sure you have put your channels in the config.json file!`);
-    console.log(err);
+    console.error("Error, please make sure you have put your channels in the config.json file!");
+    console.error(err);
 });
 
 process.on("exit", code => {
@@ -18,27 +17,25 @@ process.on("exit", code => {
     console.log(`Exited with code ${code}, up for ${Math.floor(performance.now() / 60000)} minutes`);
 });
 
-(function type(id = isArray ? 0 : null) { // Function to send typing request to channel
+(function type(id = 0) { // Function to send typing request to channel
+    const channelID = config.channel[id]
     const typing = https.request({ // Send HTTPS request
         hostname: "discord.com",
-        path: `/api/v10/channels/${isArray ? config.channel[id] : config.channel}/typing`,
+        path: `/api/v10/channels/${channelID}/typing`,
         method: "POST",
         headers: {
             Authorization: config.token
         }
     }, res => {
-        res.setEncoding("utf-8");
-        // If status code is NOT 204, an error occurred, this will not close the script
-        if (res.statusCode !== 204) return console.log(`Error sending request! Please verify that the token and channel ID ${isArray ? config.channel[id] : config.channel} is valid! Status: ${res.statusCode} (${res.statusMessage})`);
-        if (config.log) console.log(`Sent typing request to channel ${isArray ? config.channel[id] : config.channel}!`); // If status code is 204, it succeeded, log if true in config
+        if (res.statusCode !== 204) return console.error(`Error sending request! Please verify that the token and channel ID ${channelID} is valid! Status: ${res.statusCode} (${res.statusMessage})`); // If request is not successful
+        if (config.log) console.log(`Sent typing request to channel ${channelID}!`); // Log if request is successful and logging enabled in config
     });
     typing.on("error", err => {
-        // Log if their is an error sending typing request
-        console.log("HTTPS error while sending request!");
-        console.log(err);
+        // Log if HTTPS error sending typing request
+        console.error("HTTPS error while sending request!");
+        console.error(err);
     });
-    // On close, if there is another channel then send function again, else timeout for specified amount (9 seconds if null)
-    // When the typing request is sent, user/bot will show as typing for 10 seconds, after that you will have to send request again to continue
-    typing.on("close", () => isArray ? config.channel[id + 1] ? type(id + 1) : setTimeout(() => type(), config.timeout || 9000) : setTimeout(() => type(), config.timeout || 9000));
+    // When the typing request is sent Discord will show user/bot typing for 10 seconds, after that the request will have to be sent again to continue
+    typing.on("close", () => config.channel[id + 1] ? type(id + 1) : setTimeout(() => type(), config.timeout || 9000)); // On close, run tyoe function again for any more channels or wait x to repeat
     typing.end(); // End request
 })();
