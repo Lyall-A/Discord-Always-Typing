03d007eecf3bbdd4923d8506479a5cb42ccf6db3
Lyall <lyall.yeet@gmail.com>
2024-04-11 20:53:40 +0100

    just readme and comment changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const config = require(`${__dirname}/config.json`); // Config
 const https = require("https"); // HTTPS module
 if (!config.token) return console.log("No token provided, please get your Discord token and add it to the config.json file next to \"token\"!"); // If token not provided
-if (!config.channel?.[0]) return console.log("No channel ID(s) provided, please get 1 or more channel ID(s) and add it to the config.json file next to \"channel\"! Example: \x1b[100m\"channel\": [\"1234567890123456789\", \"9876543210987654321\"]\x1b[0m"); // If channel ID(s) not provided
+if (!config.channel?.[0]) return console.log("No channel ID(s) provided, please get 1 or more channel ID(s) and add it to the config.json file next to \"channel\" as an array or string!\x1b[0m"); // If channel ID(s) not provided
 if (typeof config.channel === "string") config.channel = [config.channel]; // Converts single channel ID string to array
 
 console.log(`Started! Channel ID(s): ${config.channel.join(", ")}`); // Log channel ID(s)
@@ -12,9 +12,10 @@ process.on("exit", code => {
 });
 
 (function type(id = 0) { // Function to send typing request to channel
-    const channelID = config.channel[id]
+    const channelID = config.channel[id];
+
     const typing = https.request({ // Send HTTPS request
-        hostname: "discord.com",
+        host: "discord.com",
         path: `/api/v10/channels/${channelID}/typing`,
         method: "POST",
         headers: {
@@ -24,12 +25,14 @@ process.on("exit", code => {
         if (res.statusCode !== 204) return console.error(`Error sending request! Please verify that the token and channel ID ${channelID} is valid! Status: ${res.statusCode} (${res.statusMessage})`); // If request is not successful
         if (config.log) console.log(`Sent typing request to channel ${channelID}!`); // Log if request is successful and logging enabled in config
     });
+
     typing.on("error", err => {
         // Log if HTTPS error sending typing request
         console.error("HTTPS error while sending request!");
         console.error(err);
     });
+
     // When the typing request is sent Discord will show user/bot typing for 10 seconds, after that the request will have to be sent again to continue
-    typing.on("close", () => config.channel[id + 1] ? type(id + 1) : setTimeout(() => type(), config.timeout || 9000)); // On close, run tyoe function again for any more channels or wait x to repeat
+    typing.on("close", () => config.channel[id + 1] ? type(id + 1) : setTimeout(() => type(), config.timeout || 9000)); // On close, run type function again for any more channels or wait x to repeat
     typing.end(); // End request
 })();
--- readme.md
+++ readme.md
@@ -4,24 +4,24 @@ A node.js app to make it look like you are typing forever!
 
 ## Usage
 
-Download as ZIP:
+[Download as ZIP](https://github.com/Lyall-A/Discord-Always-Typing/archive/refs/heads/main.zip)
 
 ![Download as ZIP example](https://github.com/Lyall-A/Discord-Always-Typing/blob/main/zip.png?raw=true)
 
-Open `config.json` file
+Open `config.json` file in a text editor or IDE
 
 Replace `YOUR TOKEN HERE` with your bot or user token
 
-Replace `CHANNEL ID` with your channel ID (You can have multiple)
+Replace `CHANNEL ID x` with your channel ID(s)
 
-Save and run the `start.bat` file
+Save then run the `start.bat` file or `node .` in terminal
 
 ## Config options
 
-`token`: User or bot token
+`token` `[String]`: User or bot token
 
-`channel`: String or array of channel ID(s)
+`channel` `[String/Array]`: String or array of channel ID(s)
 
-`timeout`: Delay between sending typing requests in milliseconds, default: `9000`. Discord assumes you are no longer typing after 10 seconds
+`timeout` `[Number]`: Delay between sending typing requests in milliseconds, default: `9000`. Discord assumes you are no longer typing after 10 seconds
 
-`log`: Enable logging, default: `false`
+`log` `[Boolean]`: Enable logging, default: `false`
